e2bfb97bad532581e59c48faae39db70d5e685d2
ap75 <pavel.alexeyev@gmail.com>
2025-03-02 15:41:22 +0200

    Create DB connection and data model

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from sqlalchemy import create_engine, Column, Integer, String, BigInteger
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@db:5432/population')
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+class Country(Base):
+    __tablename__ = 'countries'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    region = Column(String, nullable=False)
+    population = Column(BigInteger, nullable=False)
+
+
+def init_db():
+    Base.metadata.create_all(engine)
