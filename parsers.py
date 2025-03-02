b63602fd1b94138e1ce4e28ee521960c3b12fc50
ap75 <pavel.alexeyev@gmail.com>
2025-03-02 16:07:14 +0200

    Test wiki parsing, fix requirements for parsing

---
+++ parsers.py
@@ -0,0 +1,25 @@
+import asyncio
+import requests
+import pandas as pd
+
+
+async def fetch_data_wikipedia():
+    url = 'https://en.wikipedia.org/w/index.php?title=List_of_countries_by_population_(United_Nations)&oldid=1215058959'
+
+    response = requests.get(url)
+    tables = pd.read_html(response.text)
+
+    df = tables[0]
+    df = df.iloc[1:, [0, 4, 2]]
+    df.columns = ['country', 'region', 'population']
+
+    df['population'] = df['population'].astype('Int64')
+
+    return df[['country', 'region', 'population']]
+
+
+async def test():
+    df = await fetch_data_wikipedia()
+    print(df.head(10))
+
+asyncio.run(test())
